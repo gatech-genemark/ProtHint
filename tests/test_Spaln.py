f74751ace7351064c4afe723a1e7d13dfaf828df
Tomas Bruna <bruna.tomas@gmail.com>
2019-07-02 18:23:19 -0400

    Update tests for Spaln only runs

--- tests/test_Spaln.py
+++ tests/test_Spaln.py
@@ -12,17 +12,6 @@ import filecmp
 
 class TestSpaln(unittest.TestCase):
 
-    def testSpalnToGff(self):
-        os.chdir(testDir + "/test_Spaln")
-        command = prothint.binDir + "/spaln_to_gff.py < test.spaln --intronScore 10 \
-                  --startScore 10 --stopScore 10 --gene 289_g.fasta --prot \
-                  156304_0:00222f.fasta > single.gff"
-        subprocess.call(command, shell=True)
-
-        self.assertEqual(filecmp.cmp("result_single.gff",
-                                     "single.gff"), True)
-        os.remove("single.gff")
-
     def testSpaln(self, pbs=True):
         prothint.workDir = testDir + "/test_Spaln"
         prothint.proteins = prothint.workDir + "/proteins.fasta"
