8b7d445ace483f41ddbb673b359ed81cb9d221fd
Tomas Bruna <bruna.tomas@gmail.com>
2019-07-05 14:00:43 -0400

    Update printing of high confidence hints

--- bin/print_high_confidence.py
+++ bin/print_high_confidence.py
@@ -27,8 +27,8 @@ def intron(row, args):
 
     if not args.addAllSpliceSites:
         spliceSites = extractFeature(row[8], "splice_sites")
-        if spliceSites is not None and spliceSites != "GT_AG":
-            if not args.addGCAG or spliceSites != "GC_AG":
+        if spliceSites is not None and spliceSites.lower() != "gt_ag":
+            if not args.addGCAG or spliceSites.lower() != "gc_ag":
                 return
 
     if (((int(row[5]) >= args.intronCoverage) or
@@ -39,15 +39,33 @@ def intron(row, args):
 
 def stop(row, args):
     fullProtein = extractFeature(row[8], "fullProteinAligned")
-    if ((int(row[5]) >= args.stopCoverage) or
+    stopScore = extractFeature(row[8], "score")
+    if (stopScore is None):
+        stopScore = 1
+    else:
+        stopScore = float(stopScore)
+
+    if ((int(row[5]) >= args.stopCoverage and stopScore >= args.stopScore) or
        (args.addFullAligned and fullProtein == "TRUE")):
         print("\t".join(row))
 
 
 def start(row, args):
-    CDS_overlap = float(extractFeature(row[8], "CDS_overlap"))
+    CDS_overlap = extractFeature(row[8], "CDS_overlap")
+    if (CDS_overlap is None):
+        CDS_overlap = 0
+    else:
+        CDS_overlap = float(CDS_overlap)
+
     fullProtein = extractFeature(row[8], "fullProteinAligned")
-    if (((int(row[5]) >= args.startCoverage) or
+
+    startScore = extractFeature(row[8], "score")
+    if (startScore is None):
+        startScore = 1
+    else:
+        startScore = float(startScore)
+
+    if (((int(row[5]) >= args.startCoverage and startScore >= args.startScore) or
         (args.addFullAligned and fullProtein == "TRUE")) and
        (CDS_overlap <= args.startOverlap)):
         print("\t".join(row))
@@ -55,6 +73,9 @@ def start(row, args):
 
 def printHighConfidence(args):
     for row in csv.reader(open(args.input), delimiter='\t'):
+        if row[5] == ".":
+            row[5] = "1"
+
         if (row[2].lower() == "intron"):
             intron(row, args)
         elif (row[2].lower() == "start_codon"):
@@ -81,16 +102,24 @@ def parseCmd():
                         with coverage >= intronCoverage. Default = 4.', default=4)
     parser.add_argument('--startCoverage', type=int,
                         help='Start coverage score threshold. Print all starts \
-                        with coverage >= startCoverage. Default = 4.', default=4)
+                        with coverage >= startCoverage. Default = 10.', default=10)
     parser.add_argument('--stopCoverage', type=int,
                         help='Stop coverage score threshold. Print all stops \
                         with coverage >= stopCoverage. Default = 4.', default=4)
+    parser.add_argument('--startScore', type=float,
+                        help='Start alignment score threshold. Print all starts \
+                        with score >= startScore. Ignore if the field does not exist. \
+                        Default = 0.01.', default=0.01)
+    parser.add_argument('--stopScore', type=float,
+                        help='Stop alignment score threshold. Print all stops \
+                        with score >= stopScore. Ignore if the field does not exist. \
+                        Default = 0.01.', default=0.01)
     parser.add_argument('--intronAlignment', type=float,
                         help='Intron alignment score threshold. Print all introns \
                         with al_score >= intronAlignment. Default = 0.3.', default=0.3)
     parser.add_argument('--startOverlap', type=int,
                         help='Maximum alowed CDS overlap of a start. Print all starts \
-                        with CDS overlap <= startOverlap. Default = 3', default=3)
+                        with CDS overlap <= startOverlap. Default = 20', default=20)
     parser.add_argument('--addFullAligned', action='store_true',
                         help='Add hints with fullProteinAligned flag even if they do not \
                         satisfy the coverage threshold condition.')
